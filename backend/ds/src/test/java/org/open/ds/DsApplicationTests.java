18d2c2b6491114b150ad609a91f25847d7d9b151
maxmrzk <m.mrazek21@gmail.com>
2025-04-03 14:43:15 +0200

    restructure, add Spring boot for backend

---
+++ backend/ds/src/test/java/org/open/ds/DsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.open.ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
